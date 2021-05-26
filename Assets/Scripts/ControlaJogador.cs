72ec34aac7e1173d656d702abfc02a2c27256afe
Jefferson Vinícius Diniz Silva <j.diniz.silva@avanade.com>
2021-05-26 13:16:44 -0300

    Aula 1 e Inicio Aula 2

--- Assets/Scripts/ControlaJogador.cs
+++ Assets/Scripts/ControlaJogador.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ControlaJogador : MonoBehaviour
 {
+    public float velocidade = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,8 @@ public class ControlaJogador : MonoBehaviour
         float eixoX = Input.GetAxis("Horizontal");
         float eixoZ = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector3(eixoX, 0, eixoZ));
+        var movimentoNormalizado = (new Vector3(eixoX, 0, eixoZ)) * Time.deltaTime * velocidade;
+
+        transform.Translate(movimentoNormalizado);
     }
 }
